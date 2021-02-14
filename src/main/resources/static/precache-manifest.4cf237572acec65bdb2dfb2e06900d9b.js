6cd1a6f22e3881b394c48f34910918aa1292d899
HongPhucc <phuctran3289@gmail.com>
2021-02-14 21:19:06 +1100

    add static folder

---
+++ src/main/resources/static/precache-manifest.4cf237572acec65bdb2dfb2e06900d9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a67a1e0dccc0c0867c4c382ecc254e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "be4f62266ac37c71d972",
+    "url": "/static/css/main.d3c1742d.chunk.css"
+  },
+  {
+    "revision": "39ee9d94a6e5792d8c0d",
+    "url": "/static/js/2.46708143.chunk.js"
+  },
+  {
+    "revision": "be4f62266ac37c71d972",
+    "url": "/static/js/main.f8d3b6aa.chunk.js"
+  },
+  {
+    "revision": "a58a2d5e2e2bf522cd66",
+    "url": "/static/js/runtime-main.c2ff858f.js"
+  }
+]);
